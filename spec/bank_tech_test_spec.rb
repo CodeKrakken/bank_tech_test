bb3bc9640a543396895f55f87f35d702cebee4a7
Donald Newholm <newholm1@hotmail.com>
2019-12-17 17:09:21 +0000

    Refactor tests to automatically expect today's date

--- spec/bank_tech_test_spec.rb
+++ spec/bank_tech_test_spec.rb
@@ -4,41 +4,42 @@ describe 'account' do
 
   before(:each) do
     @account = Account.new
+    @today = Time.now.strftime("%d/%m/%Y")
   end
 
   it 'allows the account holder to credit 10' do
-    expect(@account.credit(10)).to eq("17/12/2019 || 10 || || 10")
+    expect(@account.credit(10)).to eq("#{@today} || 10 || || 10")
   end
 
   it 'keeps a running balance' do
     @account.credit(10)
-    expect(@account.credit(10)).to eq("17/12/2019 || 10 || || 20")
+    expect(@account.credit(10)).to eq("#{@today} || 10 || || 20")
   end
 
   it 'allows the account holder to debit 10' do
-    expect(@account.debit(10)).to eq("17/12/2019 || || 10 || -10")
+    expect(@account.debit(10)).to eq("#{@today} || || 10 || -10")
   end
 
   it 'keeps track of a combination of debits and credits' do
     @account.credit(10)
     @account.credit(10)
     @account.debit(5)
-    expect(@account.debit(35)).to eq("17/12/2019 || || 35 || -20")
+    expect(@account.debit(35)).to eq("#{@today} || || 35 || -20")
   end
 
   it 'returns the date along with the balance' do
-    expect(@account.credit(10)).to eq("17/12/2019 || 10 || || 10")
+    expect(@account.credit(10)).to eq("#{@today} || 10 || || 10")
   end
 
   it 'returns the transaction amount for a credit along with date and balance' do
-    expect(@account.credit(10)).to eq("17/12/2019 || 10 || || 10")
+    expect(@account.credit(10)).to eq("#{@today} || 10 || || 10")
   end
 
   it 'returns a receipt with credit column' do
-    expect(@account.credit(10)).to eq("17/12/2019 || 10 || || 10")
+    expect(@account.credit(10)).to eq("#{@today} || 10 || || 10")
   end
 
   it 'returns a receipt with debit column' do
-    expect(@account.debit(10)).to eq("17/12/2019 || || 10 || -10")
+    expect(@account.debit(10)).to eq("#{@today} || || 10 || -10")
   end
 end

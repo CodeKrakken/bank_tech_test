6cfffcdf6cec75400d0bddb796c23ab6754a8e10
Donald Newholm <newholm1@hotmail.com>
2019-12-17 08:29:08 +0000

    Refactor deposit tests

--- spec/bank_tech_test_spec.rb
+++ spec/bank_tech_test_spec.rb
@@ -1,19 +1,19 @@
 require 'bank_tech_test'
 
-# before :each do
-#   account = Account.new
-# end
-
 describe 'account' do
 
+  before(:each) do
+    @account = Account.new
+  end
+
   it 'allows the account holder to deposit 10' do
-    account = Account.new
-    expect(account.deposit(10)).to eq(10)
+    # account = Account.new
+    expect(@account.deposit(10)).to eq(10)
   end
 
   it 'keeps a running total of deposits' do
-    account = Account.new
-    account.deposit(10)
-    expect(account.deposit(10)).to eq(20)
+    # account = Account.new
+    @account.deposit(10)
+    expect(@account.deposit(10)).to eq(20)
   end
 end
